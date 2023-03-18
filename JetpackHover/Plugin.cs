39602c476e2b6efa7d7f8ff0b98d28d14abee9ce
FloppyDisk <FloppyDisk@none>
2023-03-18 20:53:10 +1300

    Jetpack Hover from decompile with IKeybind

---
+++ JetpackHover/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using PulsarModLoader;
+using PulsarModLoader.Keybinds;
+
+namespace JetpackHover
+{
+	public class Plugin : PulsarMod, IKeybind
+	{
+		public override string HarmonyIdentifier()
+		{
+			return "id107.jetpackHover";
+		}
+
+		public override string Author
+		{
+			get
+			{
+				return "18107, Fixed By Dragon";
+			}
+		}
+
+		public override string Version
+		{
+			get
+			{
+				return "0.0.9";
+			}
+		}
+
+		public override string ShortDescription
+		{
+			get
+			{
+				return "Press Left_Ctrl to toggle hover";
+			}
+		}
+
+        public void RegisterBinds(KeybindManager manager)
+        {
+            manager.NewBind("Jetpack Hover", "jetpackhoverbind", "Basics", "LEFTCONTROL");
+        }
+    }
+}
---
+++ JetpackHover/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("JetpackHover")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JetpackHover")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6f49f10e-54ef-43c4-9267-2b0c1cb25202")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
